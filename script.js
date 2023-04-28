a0b9d923afee6672d900b640eb3f2f5132a89c31
OliKirk <owk0907@gmail.com>
2023-04-28 15:42:33 +0200

    Funktions Struktur

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 "use strict";
+
+window.addEventListener("load", initApp);
+
+function initApp() {
+  console.log("initApp is runing");
+}
+
+async function updateChampsGrid() {}
+
+function getChampsData() {}
+
+function showChamps() {}
+
+function showChamp() {
+  function openChampDialog() {}
+
+  function deleteChampClicked(params) {}
+
+  function updateChampClicked(params) {}
+
+  async function viewChamp() {
+    // muligt tilføjelse af update- og deletechamp hvis layout trænges
+  }
+}
+
+function prepareChamps() {}
+
+function deleteChamp() {}
+
+function updateChamp() {}
+
+function createChamp() {}
+
+function serchChamps() {}
+
+function filterChamps() {}
+
+function closeChampDialog() {}
