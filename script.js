c710f6ae34f2451f55060fd7053784992da67d16
JaDetGodtDu <123992392+JaDetGodtDu@users.noreply.github.com>
2023-05-03 10:30:35 +0200

    Create champ function - still not working

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ function initApp() {
   console.log("initApp is running 🎉");
   updateChampsGrid();
   // document.querySelector("#dialog-update-btn").addEventListener("click", updateChamp());
+document.querySelector("#create-champ-btn").addEventListener("click", showCreateChampDialog)
+document.querySelector("#form-create-champ").addEventListener("submit", createChampClicked)
 }
 
 async function updateChampsGrid() {
@@ -92,10 +94,30 @@ function showChamp(champ) {
       updateChamp(champ);
     });
   }
-
   /* async function viewChamp() {
     // muligt tilføjelse af update- og deletechamp hvis layout trænges
   } */
+}
+function showCreateChampDialog () {
+  console.log("create champ clicked");
+document.querySelector("#dialog-create-champ").showModal()
+}
+
+async function createChampClicked (event) {
+const form = event.target;
+const name = form.name.value;
+const description = form.description.value;
+const image = form.image.value;
+const region = form.region.value;
+const sex = form.sex.value;
+const species = form.species.value;
+const role = form.role.value;
+const type = form.type.value;
+const response = await createChamp(name, description, image, region, sex, species, role, type); 
+if (response.ok) {
+  updateChampsGrid();
+  form.reset();
+  }
 }
 
 function prepareChamps(dataObject) {
@@ -146,6 +168,7 @@ async function updateChamp(name, description, image, region, sex, species, role,
 }
 
 async function createChamp(name, description, image, region, sex, species, role, type) {
+  console.log("create champ")
   const newChamp = {
     name,
     description,
@@ -165,6 +188,7 @@ async function createChamp(name, description, image, region, sex, species, role,
     console.log("New champ succesfully added to Firebase 🔥");
     updateChampsGrid();
   }
+  /* return response */
 }
 
 function searchChamps() {}
